4ba5e242f9f68d15406b044ccdd3904d736a1d78
Informatica-237 <124802214+Informatica-237@users.noreply.github.com>
2024-01-29 10:26:53 -0300

    Update Axios.js

--- src/Axios.js
+++ src/Axios.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'http://localhost:3000'; //Localhost
-// axios.defaults.baseURL = 'https://back-secretariaprivada-dev-nnxr.4.us-1.fl0.io'; //Fl0
+// axios.defaults.baseURL = 'http://localhost:3000'; //Localhost
+// axios.defaults.baseURL = 'https://back-secretariaprivada-dev-nmxr.4.us-1.fl0.io'; //Fl0
+
+// NUEVI FL0 
+axios.defaults.baseURL = 'https://back-secretariaprivada-dev-bpbs.1.us-1.fl0.io'; //Fl0
 
 
 
