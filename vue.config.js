99a639e4e26fbefd2c8f39503b55a159e4fb2bdb
Informatica-237 <124802214+Informatica-237@users.noreply.github.com>
2024-02-02 09:42:43 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,22 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = {
+  ...defineConfig({
+    transpileDependencies: true
+  }),
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://madariagasec.firebaseapp.com', // Reemplaza con la URL de tu servidor de API
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '', // Elimina /api del inicio de la URL
+        },
+      },
+    },
+  },
+};
